38c7d13ba0a89c3a315c0c7c680d3647a9f55379
Allen Liu <48594384+RyukerLiu@users.noreply.github.com>
2026-02-07 01:49:03 +0800

    Add versioned hello endpoints

--- ec/views.py
+++ ec/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -7,3 +9,25 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+@api_view(["GET"])
+def hello(request):
+    return Response(
+        {
+            "message": "hello",
+            "versions": [
+                {"version": 1, "path": "/hello/v1/"},
+                {"version": 2, "path": "/hello/v2/"},
+                {"version": 3, "path": "/hello/v3/"},
+                {"version": 4, "path": "/hello/v4/"},
+            ],
+        }
+    )
+
+
+@api_view(["GET"])
+def hello_version(request, version):
+    if version not in {1, 2, 3, 4}:
+        return Response({"detail": "Not found."}, status=404)
+    return Response({"message": "hello", "version": version})
